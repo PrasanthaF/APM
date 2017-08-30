9229d24eeba2663ce20f2b9757b92c91a42a2009
Prasantha Fernando <fprasantha@gmail.com>
2017-08-29 20:04:56 -0400

    Initial checkin

---
+++ src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from "../product";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product : IProduct;
+
+  constructor(private _route: ActivatedRoute, 
+              private _router: Router) { }
+
+  ngOnInit() {
+    let id = +this._route.snapshot.params['id'];
+    this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
+
+}
